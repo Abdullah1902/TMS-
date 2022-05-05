3a2d37594bf9a54062d585f49db25d4dff3d20d4
Abdullah <104328945+Abdullah1902@users.noreply.github.com>
2022-05-06 00:00:56 +0500

    TMS Managements
    TMS Managements

---
+++ app/src/main/java/com/example/tms_application/Splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.tms_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
